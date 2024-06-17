b47c257fcf0d0b8222c7ac507767c0b50fdea75a
YashGarg2005 <163020194+YashGarg2005@users.noreply.github.com>
2024-06-17 14:37:18 +0530

    Update employee.t.sol

--- YASH_GARG_231185/question_3/EmployeeRegistree/employee.t.sol
+++ YASH_GARG_231185/question_3/EmployeeRegistree/employee.t.sol
@@ -1,1 +1,42 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+import { Test } from "lib/forge-std/src/Test.sol";
+import { EmployeeRegistree } from "../src/employee.sol";
+contract EmployeeRegistreeTest is Test {
+    EmployeeRegistree public employee;
+    function setUp() public{
+        employee = new EmployeeRegistree();
+        employee.AddEmployee("candidate_1","GM",123);//in thousand dollars
+        employee.AddEmployee("candidate_2","VP",456);//in thousand dollars
+    }
+    function testAddEmployee() public view{
+        uint expected = 2;
+        uint actual = employee.nextID();
+        assertEq(expected, actual);
+    }
+    function testGetEmployee() public view{
+        string memory expectedName = "candidate_1";
+        string memory expectedposition = "GM";
+        uint expectedSalary = 123;
+        (string memory name, string memory position, uint salary) = employee.GetEmployee(0);
+        assertEq(expectedName, name);
+        assertEq(expectedposition, position);
+        assertEq(expectedSalary, salary);
 
+    }
+    function testUpdateEmployee() public{
+        employee.UpdateEmployee(0,"candidate_1","GM",789);
+        (string memory name, string memory position, uint salary) = employee.GetEmployee(0);
+        assertEq("candidate_1", name);
+        assertEq("GM", position);
+        assertEq(789, salary);
+
+    }
+    function testDelEmployee() public{
+        employee.DelEmployee(0);
+        uint expected = 2;
+        uint actual = employee.nextID();
+        assertEq(expected, actual);
+
+    }
+}
