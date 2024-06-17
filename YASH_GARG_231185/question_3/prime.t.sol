57221d7d4abd95114c67a30ebd49af206353c9fb
YashGarg2005 <163020194+YashGarg2005@users.noreply.github.com>
2024-06-17 13:16:49 +0530

    Update prime.t.sol

--- YASH_GARG_231185/question_3/prime.t.sol
+++ YASH_GARG_231185/question_3/prime.t.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import { Test } from "lib/forge-std/src/Test.sol";
+import { Question1 } from "../src/prime.sol";
+
+contract TestQuestion1 is Test {
+    Question1 prime;
+
+    function setUp() public {
+        prime = new Question1();
+    }
+    function test_is_prime() public view { //checking for prime number only
+        assertTrue(prime.is_prime(7));
+    }
+
+}
